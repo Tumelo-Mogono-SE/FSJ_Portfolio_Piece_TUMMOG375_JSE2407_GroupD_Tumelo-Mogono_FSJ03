e0c664fe8a7bf638f8559017fb22d4ba133b4f80
Tumelo Mogono <tumelomogono@gmail.com>
2024-09-13 11:37:07 +0200

    Created a carousel which will be used on my product cards.

---
+++ src/components/Carousol.js
@@ -0,0 +1,47 @@
+import { Carousel } from "flowbite-react";
+
+export function CarouselCompound({ images }) {
+
+    const handleControlClick = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div className="w-full h-52">
+            <Carousel 
+                leftControl={
+                    <button
+                        onClick={handleControlClick}
+                        className="bg-white/30  text-white p-2 rounded-full dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+                    >
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                        </svg>
+                    </button>
+                }
+
+                rightControl={
+                    <button
+                        onClick={handleControlClick}
+                        className="bg-white/30  text-white p-2 rounded-full dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+                    >
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                    </button>
+                }
+            >
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt={`Product image ${index + 1}`}
+                        className="object-contain w-full h-full"
+                    />
+                ))}
+            </Carousel>
+        </div>
+    );
+}
+
+
