3d52f3bf2d84bcf13cc4a8886aba5e4f68e2b3c0
Tumelo Mogono <tumelomogono@gmail.com>
2024-09-26 21:27:23 +0200

    Added a fetch request for categories and added category, sortBy, order and search into my queries.

--- src/api/api.js
+++ src/api/api.js
@@ -6,10 +6,15 @@
  * @param {number} [params.skip=0] - The number of products to skip (default is 0).
  * @returns {Promise<Object|Error>} A promise that resolves to the list of products or an error object.
  */
-export async function fetchProducts ({limit = 20, skip = 0} = {}) {
+export async function fetchProducts ({limit = 20, skip = 0, category, sortBy, order, search} = {}) {
     const query = new URLSearchParams({
         limit,
         skip,
+        ...(category && { category: encodeURIComponent(category) }),
+        ...(sortBy && { sortBy }),
+        ...(order && { order }),
+        ...(search ? { search: encodeURIComponent(search) } : {}),
+
     }).toString();
 
     try{
@@ -43,4 +48,10 @@ export async function fetchSingleProduct(productId) {
     } catch(error){
         return error
     }
+}
+
+export async function fetchCategories(){
+    const response = await fetch("https://next-ecommerce-api.vercel.app/categories");
+    const data = await response.json();
+    return data;
 }
