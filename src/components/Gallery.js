be3ffda72dc201ee84f3dc9006f6cee99837f657
Tumelo Mogono <tumelomogono@gmail.com>
2024-09-27 16:20:58 +0200

    Implemented the Image from next into the code.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -11,7 +11,7 @@ import Image from "next/image";
  * @param {string[]} props.images - An array of image URLs to be displayed in the gallery.
  * @returns {JSX.Element} - JSX to render the image gallery component.
  */
-export default function Gallery({images}){
+export default function Gallery({images}) {
 
     /**
      * selectedImage is the state that holds the currently selected image to display in the main view.
@@ -19,37 +19,44 @@ export default function Gallery({images}){
      *
      * @type {string}
      */
-    const [ selectedImage, setSelectedImage ] = useState(images[0]);
-    
+    const [selectedImage, setSelectedImage] = useState(images[0]);
 
-    return(
+    return (
         <section className="py-6 dark:bg-gray-100 dark:text-gray-900">
             <div className="container flex flex-col items-center md:flex-row md:items-start gap-4 p-4 mx-auto">
-            <div className="flex flex-row gap-1 md:flex-col">
-                    {
-                        images.map((image, index) => (
+                
+                {/* Thumbnail images */}
+                <div className="flex flex-row gap-1 md:flex-col">
+                    {images.map((image, index) => (
+                        <div 
+                            key={index} 
+                            className="relative w-24 h-24 cursor-pointer"
+                            onClick={() => setSelectedImage(image)}
+                        >
                             <Image
-                                key={index}
-                                alt={`Image ${index}`} 
-                                className="w-24 h-24 rounded shadow-lg object-contain dark:bg-gray-500 aspect-square cursor-pointer"
-                                width={96}
-                                height={96}
                                 src={image}
-                                onClick={() => setSelectedImage(image)}
+                                alt={`Product image ${index + 1}`}
+                                className="object-contain"
+                                fill
+                                sizes="100%"
+                                style={{ objectFit: 'contain' }}
                             />
-                        ))
-                    }
+                        </div>
+                    ))}
                 </div>
-                <div className="relative w-full max-w-lg h-auto aspect-w-16 aspect-h-9">
-                    <Image 
+                
+                
+                {/* Main selected image */}
+                
+                    <Image
                         src={selectedImage}
-                        alt=""
-                        layout="fill"
-                        style={{ objectFit: 'contain'}}
-                        className="rounded shadow-lg"
+                        alt={`Selected product image`}
+                        className="object-contain rounded shadow-lg"
+                        width={500}
+                        height={500}
+                        style={{ objectFit: 'contain' }}
                     />
-                </div>
-                {/* <img src={selectedImage} alt="" className="w-full h-auto max-w-lg rounded shadow-lg object-contain" /> */}
+            
             </div>
         </section>
     )
