251756b5569f5b834446ed0860e2a1b98b6d2a57
Tumelo Mogono <tumelomogono@gmail.com>
2024-09-27 16:20:37 +0200

    Adjusted style for the error page.

--- src/app/products/[id]/error.js
+++ src/app/products/[id]/error.js
@@ -8,11 +8,24 @@ export default function Error({ error, reset }) {
     }, [error]);
 
     return (
-    <div>
-        <h2>Something went wrong with this product!</h2>
-        <button onClick={() => reset()}>
-        Try again
-        </button>
-    </div>
+    <section className="flex items-center h-full p-16 dark:bg-gray-50 text-gray-800">
+            <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+                <div className="max-w-md text-center">
+                    <h2 className="mb-8 font-extrabold text-9xl text-gray-400">
+                        <span className="sr-only">Error</span>Oops!
+                    </h2>
+                    <p className="text-2xl font-semibold md:text-3xl">Something went wrong with this product!</p>
+                    <p className="mt-4 mb-8 text-gray-600">Don't worry, try refreshing or return to the homepage.</p>
+                    <div className="flex space-x-4">
+                        <button
+                            onClick={() => reset()}
+                            className="px-8 py-3 font-semibold rounded bg-gray-500 dark:bg-violet-600 dark:text-gray-50"
+                        >
+                            Try again
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
