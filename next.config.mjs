50ef4f1712cf003c1eb10c1f7774e347db324df5
Tumelo Mogono <tumelomogono@gmail.com>
2024-10-10 00:40:39 +0200

    Removed the pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { withPWA } from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -15,24 +13,4 @@ const nextConfig = {
 };
 
 
-export default withPWA({
-    ...nextConfig,
-    pwa: {
-        dest: 'public',
-        register: true, 
-        skipWaiting: true,
-        runtimeCaching: [
-            {
-                urlPattern: /^https:\/\/your-cdn-url\/.*\.(?:png|jpg|jpeg|svg|gif)$/,
-                handler: 'CacheFirst',
-                options: {
-                    cacheName: 'image-cache',
-                    expiration: {
-                    maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                    },
-                },
-            },
-        ], 
-    },
-});
+export default nextConfig;
