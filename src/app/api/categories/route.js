0b40a6d081d5819585bfd144e969312d31a4f12c
Tumelo Mogono <tumelomogono@gmail.com>
2024-10-09 13:59:41 +0200

    Created endpoint for my categories.

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,19 @@
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import app from "@/firebaseConfig";
+
+const db = getFirestore(app);
+
+export async function GET() {
+    try {
+        const docRef = doc(db, "categories", "allCategories");
+        const docSnap = await getDoc(docRef);
+
+        if (!docSnap.exists()) {
+            return new Response(JSON.stringify({ error: "Categories not found"}), { status: 404 });
+        }
+
+        return new Response(JSON.stringify(docSnap.data().categories), { status: 200 });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+    }
+}
