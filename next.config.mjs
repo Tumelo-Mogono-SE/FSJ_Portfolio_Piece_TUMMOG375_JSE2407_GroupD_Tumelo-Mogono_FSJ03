3d9a377897c75e64837ab4e8fb54b771a8a4b239
Tumelo Mogono <tumelomogono@gmail.com>
2024-10-10 00:30:09 +0200

    Installation of JWA with the fallback document page

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withPWA } from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -13,4 +15,24 @@ const nextConfig = {
 };
 
 
-export default nextConfig;
+export default withPWA({
+    ...nextConfig,
+    pwa: {
+        dest: 'public',
+        register: true, 
+        skipWaiting: true,
+        runtimeCaching: [
+            {
+                urlPattern: /^https:\/\/your-cdn-url\/.*\.(?:png|jpg|jpeg|svg|gif)$/,
+                handler: 'CacheFirst',
+                options: {
+                    cacheName: 'image-cache',
+                    expiration: {
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                    },
+                },
+            },
+        ], 
+    },
+});
