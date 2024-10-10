6c46247dc0fa658e66e69badd9cf7f1fa7a3affc
Tumelo Mogono <tumelomogono@gmail.com>
2024-10-10 20:00:57 +0200

    Added my manifest and added my favicons in my head

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,11 +19,20 @@ export const metadata = {
   title: "SwiftCart E-commerce Store",
   description: "Welcome to our e-commerce store, offering great products.",
   keywords: "e-commerce, products, buy online, shopping",
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png"></link>
+      <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png"></link>
+      <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png"></link>
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"></link>
+      <link rel="icon" type="image/png" sizes="48x48" href="/icons/favicon-48x48.png"></link>
+      <link rel="icon" type="image/png" sizes="144x144" href="/icons/ms-icon-144x144.png"></link>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
